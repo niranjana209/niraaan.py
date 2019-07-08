8eb76edee58a24829b1a0053e42471752652b4ed
niranjana209 <52314670+niranjana209@users.noreply.github.com>
2019-07-08 03:02:37 -0700

    Update char.py

--- char.py
+++ char.py
@@ -3,4 +3,4 @@ ch = input("Enter a character: ")
 if((ch>='a' and ch<= 'z') or (ch>='A' and ch<='Z')):
     print(ch, "is an Alphabet")
 else:
-    print(ch, "is not an Alphabet
+    print(ch, "is not an Alphabet")
